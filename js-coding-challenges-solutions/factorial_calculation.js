eda8c37649385fe3ef87342efa811ea45da8f5e4
Brian Baliach <brianbaliach@Brians-MacBook-Pro.local>
2023-11-01 16:39:43 +0300

    added factorial calculation recursive and iterative

--- js-coding-challenges-solutions/factorial_calculation.js
+++ js-coding-challenges-solutions/factorial_calculation.js
@@ -1,39 +1,24 @@
-function getFactorial(num) {
-  if (num <= 0) return num;
-  if (num === 1) {
-    return num;
-  }
+/**
+ *
+ * Getting factorial implementation - recursive.
+ */
+function getFactorialRecursive(num) {
+  if (num === 1) return num;
 
-  return getFactorial(num - 1) * num;
+  return getFactorialRecursive(num - 1) * num;
 }
 
-console.log(getFactorial(5))
+/**
+ * Getting factorial implementation - iterative
+ */
+function getFactorialIterative(num) {
+  let product = 1;
+  for (let i = 1; i <= num; i++) {
+    product = product * i;
+  }
 
+  return product;
+}
 
-// num = 5;
-
-// first run:
-// input: 5
-// output: getFactorial(4) * 5
-// previous output: 24
-// final output: 24 * 5 = 120
-
-// input: 4
-// output: getFactorial(3) * 4
-// previous output: 6
-// final output: 6 * 4 = 24
-
-// input: 3
-// output: getFactorial(2) * 3
-// previous output: 2
-// final output: 2 * 3 = 6
-
-
-// input: 2
-// output: getFactorial(1) * 2
-// previous output: 1
-// final output: 1 * 2 = 2
-
-// second run:
-// input 1:
-// output: 1 * 1;
+console.log("Recursive: ", getFactorialRecursive(15));
+console.log("Iterative: ", getFactorialIterative(15));
