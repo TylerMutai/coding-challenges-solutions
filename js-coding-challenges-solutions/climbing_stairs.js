4e062022ade37aa034320432ec1bfa68fd8ff614
Brian Baliach <brianbaliach@Brians-MacBook-Pro.local>
2023-12-31 19:35:43 +0300

    added a sub-optimal solution for climbing stairs

--- js-coding-challenges-solutions/climbing_stairs.js
+++ js-coding-challenges-solutions/climbing_stairs.js
@@ -4,6 +4,24 @@
  * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
  */
 
-const climbStairs = (n)=>{
+const climbStairs = (n) => {
+  if (n === 1) {
+    return n;
+  }
+  if (n === 2) {
+    return n;
+  }
+  let prevTotal = 1;
+  let currTotal = 2;
+  for (let i = 3; i < n; i++) {
+    let temp = prevTotal + currTotal;
+    prevTotal = currTotal;
+    currTotal = temp;
+    console.log("prevTotal: ", prevTotal);
+    console.log("currTotal: ", currTotal);
+    console.log("TEMP: ", temp);
+  }
+  return prevTotal + currTotal;
+};
 
-}
+console.log(climbStairs(10));
