9f355882a9e591800b29274708e1929158a91c8c
Brian Baliach <brianbaliach@Brians-MacBook-Pro.local>
2024-01-12 08:48:14 +0300

    added climbing stairs coding challenge optimized

--- js-coding-challenges-solutions/climbing_stairs.js
+++ js-coding-challenges-solutions/climbing_stairs.js
@@ -17,11 +17,35 @@ const climbStairs = (n) => {
     let temp = prevTotal + currTotal;
     prevTotal = currTotal;
     currTotal = temp;
-    console.log("prevTotal: ", prevTotal);
-    console.log("currTotal: ", currTotal);
-    console.log("TEMP: ", temp);
   }
   return prevTotal + currTotal;
 };
 
-console.log(climbStairs(10));
+/**
+ *
+ * @param n
+ * @return {number|*}
+ * @private
+ *
+ * originally, the time complexity for this function is O(2^n).
+ * Since there's alot of repeated work, i.e. if n was 5, we've already computed for n=4,n=3,n=2,n=1.
+ * Therefore, we can skip that. Given this optimization, the time complexity becomes O(n).
+ *
+ * an Example: if n was 7 = we'll only compute for n=6,n=5 and so on.
+ * Therefore, if we started with n as 1, and we cached this result, then when n say became 5, we don't have to start
+ *   from 1.
+ */
+const _climbStairsDP = (n) => {
+  let one = 1, two = 1;
+  n--;
+
+  while (n) {
+    const temp = one;
+    one = one + two;
+    two = temp;
+    n--;
+  }
+  return one;
+};
+// console.log(climbStairs(10));
+console.log(_climbStairsDP(3));
