aa1e1b745827b5728e19931f38a8d615aa0e7675
Brian Baliach <brianbaliach@Brians-MacBook-Pro.local>
2024-02-15 13:24:59 +0300

    added an openai assistant runner that parses files in a directory

--- testing.ts
+++ testing.ts
@@ -1,4 +1,51 @@
-console.log(Buffer.from(
-  "eyJhbGciOiJIUzUxMiJ9.eyJkdmMiOiJmZWQ4NTMwMi00MjYzLTQ2MWEtOGYwOS0zYTQ2NTMyMGQzMDciLCJyZXMiOlsxODRdLCJ0a24iOiIxMzE5fjExMH4xMDQ5NX5EaWdpZmFybSIsInNpZCI6ImZlZDg1MzAyLTQyNjMtNDYxYS04ZjA5LTNhNDY1MzIwZDMwNyIsInN1YiI6ImJiYWxpYWNoIiwiaWF0IjoxNzA3NzQwODM2LCJleHAiOjE3MDc3NDQ0MzZ9.w7r8mvCpg3VWNU7EFUqsvR-6CNbE8G2nG_OO4InwxpbbSnhGkFpgP7Jh5zDnTpzeAIltaRNFI0EChzUnpec1Pg",
-  "base64"
-).toString());
+import {end} from "cheerio/lib/api/traversing";
+
+function sleep(){
+  return new Promise(resolve=>{
+    setTimeout(resolve,200);
+  })
+}
+
+async function makeIterator(
+  filePath: string,
+  start: number,
+  end: number,
+  byteLengthToRead: number,
+  ceiling: number
+) {
+  console.log("START BEFORE: ",start);
+  console.log("END BEFORE: ",end);
+  console.log("BYTE LENGTH TO READ BEFORE: ",byteLengthToRead);
+  console.log("CEILING BEFORE: ",ceiling);
+  console.log("###########: ");
+  if (start < ceiling || start >= end) {
+    // we have read the whole file. return.
+    return;
+  }
+   await sleep();
+  // move [byteLengthToRead] bytes behind.
+  let _start = start;
+  const _end = _start;
+  _start = _start - byteLengthToRead;
+
+  // if moving [byteLengthToRead] bytes behind becomes less than [ceiling], then move our
+  // [_start] to [ceiling].
+  if (_start < ceiling) {
+    _start = ceiling;
+  }
+  console.log("START AFTER: ",_start);
+  console.log("END AFTER: ",_end);
+  console.log("BYTE LENGTH TO READ: ",byteLengthToRead);
+  console.log("CEILING: ",ceiling);
+  console.log("###########: ");
+
+  await makeIterator(filePath, _start, _end, byteLengthToRead, ceiling);
+}
+const _byteLengthToRead = 13;
+const _ceiling = 1500;
+const _maxSize = 2000;
+let _end = _maxSize;
+let _start = _end - _byteLengthToRead;
+
+
+makeIterator("test",_start,_end,_byteLengthToRead,_ceiling);
---
+++ ts-coding-challenges-solutions/openAIAssistantsRunner/openAIAssistantsRunner.ts
@@ -0,0 +1,118 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import {OpenAI} from "openai";
+
+const extensionsToProcess = new Set<string>([
+  ".ts",
+]);
+
+const assistants = {
+  digifarmCodeGenerationAIApolloServerTests: "asst_SZ2onEWh0X5oycXDQ6uXlgde",
+};
+
+const openAiTool = new OpenAI();
+
+function sleep(time: number) {
+  return new Promise(resolve => {
+    setTimeout(resolve, time);
+  });
+}
+
+async function processFilesRecursively(source: string, target: string) {
+  const files = fs.readdirSync(source);
+
+  for (const file of files) {
+    const sourcePath = path.join(source, file);
+    const extension = path.extname(file);
+    const targetPath = path.join(target, `${path.basename(file, extension)}.test${extension}`);
+
+    console.log("Source destination is defined as: ", sourcePath);
+    console.log("Target destination is defined as: ", targetPath);
+
+    if (fs.statSync(sourcePath).isDirectory()) {
+      if (!fs.existsSync(targetPath)) {
+        console.log("Creating directory now at: ", targetPath);
+        fs.mkdirSync(targetPath);
+      }
+      await processFilesRecursively(sourcePath, targetPath);
+    } else {
+      console.log("Handling file: ", sourcePath);
+      if (extensionsToProcess.has(extension)) {
+
+        // if file has extension that is in  [extensionsToProcess], read the whole file.
+        const content = fs.readFileSync(sourcePath, 'utf8');
+
+        // make request to our apollo server assistant model to generate tests.
+        const aiThread = await openAiTool.beta.threads.create({
+          messages: [
+            {
+              role: "user",
+              content: "```typescript\n" + content + "\n```",
+            },
+          ],
+        });
+
+        console.log("Created thread with id: ", aiThread.id);
+        console.log("Executing run....");
+        const aiRun = await openAiTool.beta.threads.runs.create(aiThread.id, {
+          assistant_id: assistants.digifarmCodeGenerationAIApolloServerTests,
+        });
+
+        while (true) {
+          const apiRunObj = await openAiTool.beta.threads.runs.retrieve(aiThread.id, aiRun.id);
+          console.log("Checking run status. Current status: ", apiRunObj.status);
+
+          if (apiRunObj.status === "completed") {
+            try {
+              console.log("Run status is complete. Getting message and storing to file..... ");
+              const messages = await openAiTool.beta.threads.messages.list(aiThread.id);
+              const aiResponse = messages.getPaginatedItems().filter(m => m.role === "assistant");
+              const messageStrings = [];
+              for (const m of aiResponse) {
+                const _messages = m.content.filter(c => c.type === "text").map(c => (c as any).text.value);
+                messageStrings.push(..._messages);
+              }
+              console.log("The AI responded with: ", messageStrings);
+              fs.writeFileSync(targetPath, messageStrings.join("\n"), {
+                flag: "a+",
+              });
+              console.log("Successfully written contents to file: ", targetPath);
+            } catch (e) {
+              console.error("Could not write contents to file: ", targetPath);
+              console.error("The error was: ", e);
+            }
+            break;
+          }
+
+          console.log("Sleeping for 5 seconds before checking server again.");
+          await sleep(5000);
+        }
+      } else {
+        console.log("The file: ", targetPath, " is currently not supported");
+      }
+    }
+  }
+
+  console.log("File processing complete.");
+}
+
+function main() {
+  const sourceDirectory = process.argv[2];
+  const targetDirectory = process.argv[3];
+
+  if (!fs.existsSync(sourceDirectory)) {
+    console.error(`Source directory: ${sourceDirectory} does not exist.`);
+    return;
+  }
+
+  if (!fs.existsSync(targetDirectory)) {
+    console.info(`Target directory: ${targetDirectory} does not exist. Creating it now...`);
+    fs.mkdirSync(targetDirectory);
+  }
+
+  processFilesRecursively(sourceDirectory, targetDirectory).then(() => {
+    console.log("Exiting.......");
+  });
+}
+
+main();
